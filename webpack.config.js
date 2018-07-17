7811b409e15d6a36e0847ef43a15afed2b0eee9c
Denis Chaplygin <dchaplyg@redhat.com>
2018-07-17 17:29:37 +0200

    Uodated build scripts to support production/dev builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,56 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const merge = require('webpack-merge');
 
-var config = {
-    context: __dirname + '/src', // `__dirname` is root of project and `src` is source
-    entry: {
-        app: ['babel-polyfill', './js/app.js']
-    },
-    output: {
-        path: __dirname + '/dist', // `dist` is the destination
-        filename: 'bundle.js'
-    },
+var build = {
+  context: path.resolve(__dirname, 'src'), // `__dirname` is root of project and `src` is source
+  entry: {
+      app: ['babel-polyfill', './js/app.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    /*new HtmlWebpackPlugin({
+     title: 'Moi.Den.Gi'
+   }),*/
+   new webpack.HotModuleReplacementPlugin()
+ ],
+  output: {
+      path: path.resolve(__dirname, 'dist'), // `dist` is the destination
+      filename: 'bundle.js'
+  },
+  module: {
+      rules: [
+          {
+              enforce: "pre",
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "eslint-loader",
+          },
+          {
+              test: /\.js$/, // Check for all js files
+              exclude: /node_modules/,
+              use: [{
+                  loader: 'babel-loader'
+              }]
+          },
+          {
+              test: /\.css$/,
+              loader: 'style-loader!css-loader?modules',
+              include: /flexboxgrid/
+          }
+      ]
+  }
+}
+
+var development = {
+    mode: 'development',
+    devtool: "eval-source-map",
     devServer: {
-        contentBase: __dirname + '/src', // `__dirname` is root of the project
+        contentBase: path.resolve(__dirname, 'src'), // `__dirname` is root of the project
+        hot: true,
         proxy: {
             '/api': {
                 target: 'http://localhost:9000/api',
@@ -18,33 +58,21 @@ var config = {
             }
         }
     },
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-            },
-            {
-                test: /\.js$/, // Check for all js files
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader'
-                }]
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader?modules',
-                include: /flexboxgrid/
-            }
-        ]
-    },
-    devtool: "eval-source-map"
 };
 
+var production = {
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+     new UglifyJSPlugin({
+       sourceMap: true
+     })
+    ]
+}
 if (process.env.NODE_ENV === "production") {
-    config.devtool = "";
+  var config = merge(build, production)
+} else {
+  config = merge(build, development)
 }
 
 module.exports = config;
