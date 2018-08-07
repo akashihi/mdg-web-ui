f80a25fb13c78ccfe4dd4e91dd73c83cff90ea06
Denis Chaplygin <dchaplyg@redhat.com>
2018-08-07 16:50:13 +0200

    Tuned webpack to use docker-compose based API url

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ var build = {
     new HtmlWebpackPlugin({
      template: 'index.html'
    }),
-   new webpack.HotModuleReplacementPlugin(),
    new CopyWebpackPlugin([{ from: 'css', to: 'css' }], {})
  ],
   output: {
@@ -53,10 +52,10 @@ var development = {
     devtool: "eval-source-map",
     devServer: {
         contentBase: path.resolve(__dirname, 'src'), // `__dirname` is root of the project
-        hot: true,
+        watchContentBase: true,
         proxy: {
             '/api': {
-                target: 'http://localhost:9000/api',
+                target: 'http://127.0.0.1/api',
                 pathRewrite: {'^/api' : ''}
             }
         }
