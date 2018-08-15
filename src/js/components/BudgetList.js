9adc36bdc0bc707acaaa48648ef6b9c9a5013364
Denis Chaplygin <dchaplyg@redhat.com>
2018-08-15 12:12:08 +0200

    Enabled BudgetList back.

--- src/js/components/BudgetList.js
+++ src/js/components/BudgetList.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-/*import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
-import Divider from 'material-ui/Divider';
-import Drawer from 'material-ui/Drawer';
-import DatePicker from 'material-ui/DatePicker';
-import ClipLoader from 'react-spinners/ClipLoader';*/
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import Backspace from '@material-ui/icons/Backspace';
+import Divider from '@material-ui/core/Divider';
+import ClipLoader from 'react-spinners/ClipLoader';
+import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
+/*import DatePicker from 'material-ui/DatePicker';*/
 
-//import Budget from './Budget'
+import Budget from './Budget'
 
 export default class BudgetList extends Component {
     onCloseBudgetListClick() {
@@ -26,15 +27,15 @@ export default class BudgetList extends Component {
     }
 
     render() {
-        //var props = this.props;
-        /*var onSelectBudgetClick = function (budget) {
+        var props = this.props;
+        var onSelectBudgetClick = function (budget) {
             props.actions.selectBudget(budget)
         };
         var onDeleteBudgetClick = function (id) {
             props.actions.deleteBudget(id)
-        };*/
+        };
 
-        /*var budgets;
+        var budgets;
         if (props.waiting) {
             budgets = <ClipLoader sizeUnit={'px'} size={150} loading={true}/>
         } else if (props.error) {
@@ -45,20 +46,19 @@ export default class BudgetList extends Component {
                     <Budget budget={item} key={item.id} deleteFunc={onDeleteBudgetClick} selectFunc={onSelectBudgetClick}/>
                 )
             });
-        }*/
-        return <div/>
-        /*return (
-            <Drawer open={props.visible} width='25%'>
-                <IconButton style={{float: 'right'}} onClick={::this.onCloseBudgetListClick}><FontIcon
-                    className='material-icons'>backspace</FontIcon></IconButton>
+        }
+
+        return (
+            <Drawer open={props.visible}>
+              <IconButton onClick={::this.onCloseBudgetListClick}><Backspace/></IconButton>
+                {/*<IconButton style={{float: 'right'}}></IconButton>
                 <DatePicker hintText='Budget beginning' container='inline' onChange={::this.onAcceptPeriodBegin}/>
-                <DatePicker hintText='Budget end' container='inline' onChange={::this.onAcceptPeriodEnd}/>
-                <IconButton disabled={!props.valid} onClick={::this.onCreateBudgetClick}><FontIcon
-                    className='material-icons'>add_circle_outline</FontIcon></IconButton>
+                <DatePicker hintText='Budget end' container='inline' onChange={::this.onAcceptPeriodEnd}/>*/}
+                <IconButton disabled={!props.valid} onClick={::this.onCreateBudgetClick}><AddCircleOutline/></IconButton>
                 {props.formError}
                 <Divider/>
                 {budgets}
             </Drawer>
-        )*/
+        )
     }
 }
