ed0c4d53db89c029d4a582fc28dc3403abab2ed1
Denis Chaplygin <akashihi@gmail.com>
2019-07-25 20:54:31 +0300

    Added language setting

--- src/js/reducers/SettingReducer.js
+++ src/js/reducers/SettingReducer.js
@@ -4,6 +4,7 @@ import {GET_SETTING_REQUEST, GET_SETTING_SUCCESS, GET_SETTING_FAILURE} from '../
 const initialState = Map({
     primaryCurrency: -1,
     closeTransactionDialog: true,
+    language: 'en-US',
     ui: Map({
         settingListLoading: true,
         settingListError: false
@@ -18,6 +19,7 @@ export default function currencyReducer(state = initialState, action) {
         case GET_SETTING_SUCCESS:
             return state.set('primaryCurrency', parseInt(action.payload.get('currency.primary').get('value')))
                 .set('closeTransactionDialog', action.payload.get('ui.transaction.closedialog').get('value') === 'true')
+                .set('language', action.payload.get('ui.language').get('value'))
                 .setIn(['ui', 'settingListLoading'], false)
                 .setIn(['ui', 'settingListError'], false);
         case GET_SETTING_FAILURE:
