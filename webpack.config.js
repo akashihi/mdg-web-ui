301b5fb990629e6c3784d468513186256b41ec93
Denis Chaplygin <akashihi@gmail.com>
2019-07-29 22:53:35 +0300

    Switched from uglify to terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,52 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ArchivePlugin = require('webpack-archive-plugin');
 const merge = require('webpack-merge');
 
-var build = {
-  context: path.resolve(__dirname, 'src'), // `__dirname` is root of project and `src` is source
-  entry: {
-      app: ['./js/app.js']
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-     template: 'index.html'
-   }),
-   new CopyWebpackPlugin([{ from: 'css', to: 'css' }], {})
- ],
-  output: {
-      path: path.resolve(__dirname, 'dist'), // `dist` is the destination
-      filename: 'bundle.[hash].js'
-  },
-  module: {
-      rules: [
-          {
-              enforce: 'pre',
-              test: /\.js$/,
-              exclude: /node_modules/,
-              loader: 'eslint-loader',
-          },
-          {
-              test: /\.js$/, // Check for all js files
-              exclude: /node_modules/,
-              use: [{
-                  loader: 'babel-loader'
-              }]
-          },
-          {
-            test: /\.css$/,
-            include: /node_modules/,
-            loaders: ['style-loader', 'css-loader'],
-          }
-      ]
-  }
+const build = {
+    context: path.resolve(__dirname, 'src'), // `__dirname` is root of project and `src` is source
+    entry: {
+        app: ['./js/app.js']
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyWebpackPlugin([{from: 'css', to: 'css'}], {})
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'), // `dist` is the destination
+        filename: 'bundle.[hash].js'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+            },
+            {
+                test: /\.js$/, // Check for all js files
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader'
+                }]
+            },
+            {
+                test: /\.css$/,
+                include: /node_modules/,
+                loaders: ['style-loader', 'css-loader'],
+            }
+        ]
+    }
 };
 
-var development = {
+const development = {
     mode: 'development',
     devtool: 'eval-source-map',
     devServer: {
@@ -63,20 +62,19 @@ var development = {
     },
 };
 
-var production = {
+const production = {
     mode: 'production',
     devtool: 'none',
     plugins: [
-     new UglifyJSPlugin({
-       sourceMap: false
-     }),
-     new ArchivePlugin()
+        new TerserPlugin(),
+        new ArchivePlugin()
     ]
 };
+let config;
 if (process.env.NODE_ENV === 'production') {
-  var config = merge(build, production)
+    config = merge(build, production)
 } else {
-  config = merge(build, development)
+    config = merge(build, development)
 }
 
 module.exports = config;
